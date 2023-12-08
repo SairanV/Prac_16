d714b936df740f01509ae75fdf5708dca51143da
aidyn <aidynmaskutov2004@gmail.com>
2023-12-08 20:55:23 +0600

    Добавьте файлы проекта.

---
+++ Prac_16/FileChangesLogger.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac_16
+{
+    public class FileChangesLogger
+    {
+        private string directoryToWatch;
+        private string logFilePath;
+        private FileSystemWatcher watcher;
+
+        public FileChangesLogger(string directoryToWatch, string logFilePath)
+        {
+            this.directoryToWatch = directoryToWatch;
+            this.logFilePath = logFilePath;
+
+            watcher = new FileSystemWatcher(directoryToWatch);
+
+            watcher.IncludeSubdirectories = true;
+
+            watcher.Created += OnFileChanged;
+            watcher.Deleted += OnFileChanged;
+            watcher.Renamed += OnFileRenamed;
+            watcher.Changed += OnFileChanged;
+        }
+
+        /// <summary>
+        /// Обработчик события создания, удаления и изменения файла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnFileChanged(object sender, FileSystemEventArgs e)
+        {
+            LogChange($"[{DateTime.Now}] {e.ChangeType} - {e.FullPath}");
+        }
+
+        /// <summary>
+        /// Обработчик события переименования файла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnFileRenamed(object sender, RenamedEventArgs e)
+        {
+            LogChange($"[{DateTime.Now}] {e.ChangeType} - {e.OldFullPath} -> {e.FullPath}");
+        }
+
+        /// <summary>
+        /// Метод для записи изменений в лог-файл
+        /// </summary>
+        /// <param name="logEntry"></param>
+        private void LogChange(string logEntry)
+        {
+            try
+            {
+                File.AppendAllText(logFilePath, logEntry + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при записи в лог: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Метод для запуска отслеживания изменений
+        /// </summary>
+        public void StartWatching()
+        {
+            try
+            {
+                Console.WriteLine($"Отслеживание изменений в директории {directoryToWatch}. Для остановки нажмите Enter.");
+
+                watcher.EnableRaisingEvents = true;
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при запуске отслеживания: {ex.Message}");
+            }
+            finally
+            {
+                watcher.EnableRaisingEvents = false;
+            }
+        }
+    }
+}
---
+++ Prac_16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Приложение для логирования изменений в файлах");
+
+                Console.Write("Введите путь к отслеживаемой директории: ");
+                string directoryToWatch = Console.ReadLine();
+
+                Console.Write("Введите путь к лог-файлу: ");
+                string logFilePath = Console.ReadLine();
+
+                FileChangesLogger fileChangesLogger = new FileChangesLogger(directoryToWatch, logFilePath);
+                fileChangesLogger.StartWatching();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
